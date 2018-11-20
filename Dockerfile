dbad85ec6d3b71339b72aae8822fca0d493100ed
Dave Stimpert <stimpert@gmail.com>
2018-11-20 08:15:18 -0500

    spring profiles

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM openjdk:10-jre
 RUN mkdir /app
 ADD build/libs/shippable-demo-0.0.1-SNAPSHOT.jar /app/
+ENV FOO Dave
+ENV SPRING_PROFILE local
 EXPOSE 8080
-ENTRYPOINT ["java","-jar","/app/shippable-demo-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", \
+     "-Dspring.profiles.active=${SPRING_PROFILE}", \
+    "-jar","/app/shippable-demo-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/stimpert/shippable/shippabledemo/MyRestController.java
+++ src/main/java/com/stimpert/shippable/shippabledemo/MyRestController.java
@@ -1,19 +1,27 @@
 package com.stimpert.shippable.shippabledemo;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Stream;
+
 @RestController
 public class MyRestController {
+
+    @Autowired
+    private Environment env;
 
     @RequestMapping("/person")
     public Person getPerson(
             @RequestParam(value="first", defaultValue="first") String first,
             @RequestParam(value="last", defaultValue="last") String last
             ) {
-        return new Person(first, last);
+        return new Person(first, last + " - " + String.join(",", env.getActiveProfiles()));
     }
 
 }
